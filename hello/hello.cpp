f7521f93f217d69d8abbbde57b15adaeb355d5fd
Mao Yongjun <yongjun.mao@outlook.com>
2019-09-28 21:24:22 +0800

    搞定git log HEAD..ccs/master

--- hello/hello.cpp
+++ hello/hello.cpp
@@ -5,8 +5,18 @@
 #include <git2.h>
 
 const char* path = R"(C:\Users\ymao\source\repos\libgit2_study)";
-const char* remote_url = "git@github.com:waiting4love/libgit2_study.git";
+const char* remote_url = "https://github.com/waiting4love/libgit2_study.git";
 const char* remote_name = "ccs"; // c central server
+
+void check_error(int error)
+{
+	if (error < 0) {
+		const git_error *e = giterr_last();
+		printf("Error %d/%d: %s\n", error, e->klass, e->message);
+		exit(error);
+	}
+}
+
 int main()
 {
 	git_libgit2_init();
@@ -19,11 +29,7 @@ int main()
 		error = git_repository_init(&repo, path, false);
 	}
 
-	if (error < 0) {
-		const git_error *e = giterr_last();
-		printf("Error %d/%d: %s\n", error, e->klass, e->message);
-		exit(error);
-	}
+	check_error(error);
 
 	// 得到当前的remote
 	git_remote *remote = NULL;
@@ -33,22 +39,44 @@ int main()
 		error = git_remote_create(&remote, repo, remote_name, remote_url);
 	}
 
-	if (error < 0) {
-		const git_error *e = giterr_last();
-		printf("Error %d/%d: %s\n", error, e->klass, e->message);
-		exit(error);
-	}
+	check_error(error);
 
-	// 怎么得到对比
+	// 与对比远程，先取远程数据
 	error = git_remote_fetch(remote, NULL, NULL, NULL);
-	if (error < 0) {
-		const git_error *e = giterr_last();
-		printf("Error %d/%d: %s\n", error, e->klass, e->message);
-		exit(error);
-	}
+	check_error(error);
 
-	// diff
+	// 单独显示指定的提交信息
+	git_commit *head_commit = NULL;
+	error = git_revparse_single((git_object**)&head_commit, repo, "HEAD");	
+	check_error(error);
+	printf("%s", git_commit_message(head_commit));
+	git_commit_free(head_commit);
+
+	git_commit *remote_header = NULL;
+	error = git_revparse_single((git_object**)&remote_header, repo, "ccs/master");
+	check_error(error);
+	printf("%s", git_commit_message(remote_header));
+	git_commit_free(remote_header);
+
+	// git log HEAD..ccs/master --oneline 显示版本变化
+	git_revwalk *walker;
+	error = git_revwalk_new(&walker, repo);
+	check_error(error);
+	error = git_revwalk_push_range(walker, "HEAD..ccs/master");
+	check_error(error);
+	git_oid oid;
+	while (!git_revwalk_next(&oid, walker)) {
+		git_commit *c = NULL;
+		error = git_commit_lookup(&c, repo, &oid);
+		check_error(error);
+		printf("%s", git_commit_message(c));
+		git_commit_free(c);
+	}
+	git_revwalk_free(walker);
+
+	// git diff --stat master ccs/master  显示修改
 	
+
 	git_remote_free(remote);
 	git_repository_free(repo);
 	git_libgit2_shutdown();
